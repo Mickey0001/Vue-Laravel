b937f80266dde8e91c04ec7fffcd2e3605c91760
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-26 13:10:31 -0400

    Add pagination

--- resources/assets/js/components/Articles.vue
+++ resources/assets/js/components/Articles.vue
@@ -4,6 +4,9 @@
     <nav aria-label="Page navigation example">
       <ul class="pagination">
         <li v-bind:class="[{disabled: !pagination.previous_page_url}]" class="page-item"><a @click="fetchArticles(pagination.prev_page_url)" class="page-link" href="#">Previous</a></li>
+
+        <li class="page-item disabled"><a class="page-link tex-dark" href="">Page {{ pagination.current_page }} of {{ pagination.last_page }}</a></li>
+
         <li  v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item"><a @click="fetchArticles(pagination.next_page_url)" class="page-link" href="#">Next</a></li>
       </ul>
     </nav>
